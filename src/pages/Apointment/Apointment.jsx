fa539e732086dfd5117e3b521c6ae50ea4dfad73
berchiche willem <berwillempro@gmail.com>
2024-06-12 04:34:48 +0100

    forms integrated with email js

--- src/pages/Apointment/Apointment.jsx
+++ src/pages/Apointment/Apointment.jsx
@@ -1,16 +1,72 @@
 import { Link } from "react-router-dom";
 import "./Apointment.css";
 import { useState } from "react";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
+
 const Apointment = () => {
   const [fileName, setFileName] = useState("Aucun fichier sélectionné");
+  const [formValues, setFormValues] = useState({
+    nom: "",
+    prenom: "",
+    courriel: "",
+    telephone: "",
+    immatriculation: "",
+    assurance: "",
+    date: "",
+    heure: "",
+  });
 
   const handleFileChange = (event) => {
     if (event.target.files.length > 0) {
       setFileName(event.target.files[0].name);
+      setFormValues({ ...formValues, photo: event.target.files[0].name });
     } else {
       setFileName("Aucun fichier sélectionné");
+      setFormValues({ ...formValues, photo: "Aucun fichier sélectionné" });
     }
   };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .send(
+        "service_3hk2wmo",
+        "template_1b7ad7w",
+        formValues,
+        "UYlVarH1R1qY_zlE9"
+      )
+      .then(() => {
+        Swal.fire({
+          title: "Opération reussie!",
+          text: "Votre message a ete envoyer avec succes!",
+          icon: "success",
+        });
+        setFormValues({
+          nom: "",
+          prenom: "",
+          courriel: "",
+          telephone: "",
+          immatriculation: "",
+          assurance: "",
+          date: "",
+          heure: "",
+        });
+        () => {
+          Swal.fire({
+            title: "Échec de l'opération!",
+            text: "Une erreur s'est produite. Veuillez réessayer.",
+            icon: "error",
+          });
+        };
+      });
+  };
+
   return (
     <div className="container apointment">
       <h1>
@@ -21,40 +77,80 @@ const Apointment = () => {
         Pour une bonne prise en charge de votre demande, nous vous prions
         <span>de remplir correctement l’ensemble des éléments demandés</span>
       </p>
-      <form>
-       <div className="info">
-       <input type="text" placeholder="Nom*" />
-        <input type="text" placeholder="Prénom*" />
-       </div>
+      <form onSubmit={handleSubmit}>
         <div className="info">
-        <input type="text" placeholder="Courriel*" />
-        <input type="text" placeholder="Téléphone*" />
+          <input
+            type="text"
+            name="nom"
+            placeholder="Nom*"
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="prenom"
+            placeholder="Prénom*"
+            onChange={handleInputChange}
+          />
         </div>
-        
-        <input type="text" placeholder="Immatriculation*" />
-        <input type="text" placeholder="Votre assurance*" />
-       
+        <div className="info">
+          <input
+            type="text"
+            name="courriel"
+            placeholder="Courriel*"
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="telephone"
+            placeholder="Téléphone*"
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <input
+          type="text"
+          name="immatriculation"
+          placeholder="Immatriculation*"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="assurance"
+          placeholder="Votre assurance*"
+          onChange={handleInputChange}
+        />
+
         <p className="inputTitre">Date de rendez-vous</p>
-     <div className="info">
-     <input type="date" />
-        <input type="time" />
-     </div>
-        <p className="inputTitre">La date vous sera confirmées par la carrosserie</p>
+        <div className="info">
+          <input type="date" name="date" onChange={handleInputChange} />
+          <input type="time" name="heure" onChange={handleInputChange} />
+        </div>
+        <p className="inputTitre">
+          La date vous sera confirmées par la carrosserie
+        </p>
         <h2 className="SousTitre">Photos du véhicule</h2>
-     
-        
-       <div className="file-upload">
-       <input type="file" id="file" className="file-input"  onChange={handleFileChange}  />
-      <label htmlFor="file" className="file-label">Sélectionnez un fichier</label>
-      <p> {fileName}</p>
-       </div>
+
+        <div className="file-upload">
+          <input
+            type="file"
+            id="file"
+            className="file-input"
+            onChange={handleFileChange}
+          />
+          <label htmlFor="file" className="file-label">
+            Sélectionnez un fichier
+          </label>
+          <p>{fileName}</p>
+        </div>
         <p id="mention">
           J’ai lu et j’accepte les termes du site div{" "}
           <span>
             <Link to="/legal">Mentions légales</Link>
           </span>
         </p>
-       <div className="btn"> <button>JE PREND UN RENDEZ-VOUS</button></div>
+        <div className="btn">
+          <button type="submit">JE PREND UN RENDEZ-VOUS</button>
+        </div>
       </form>
     </div>
   );
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,39 @@
 import "./Contact.css";
 import { Home, Smartphone, Mail, Instagram } from "lucide-react";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
 const Contact = () => {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_3hk2wmo",
+        "template_qsunuez",
+        e.target,
+        "UYlVarH1R1qY_zlE9"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "Your message has been sent successfully!",
+          });
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Oops... Something went wrong. Please try again later.",
+          });
+        }
+      );
+  }
+
   return (
     <>
       <div className="container contact-container">
@@ -32,16 +65,21 @@ const Contact = () => {
               width="550"
               height="350"
               border="none"
-              allowfullscreen
               src="https://www.google.com/maps/embed/v1/place?q=https%3A%2F%2Fwww.google.com%2Fmaps%2Fplace%2FOptimale%2BCarrosserie%2F%4043.2772505%2C5.4159027%2C17z%2Fdata%3D!3m1!4b1!4m6!3m5!1s0x12c9b934f1988087%3A0x50df5bc91d3012a7!8m2!3d43.2772505!4d5.4159027!16s%252Fg%252F11p_5t3nkw%3Fentry%3Dttu&key=AIzaSyD3GOujWaSttqYJ6Iiw6L-ZFQfO7YzNpeM"
             ></iframe>
           </div>
         </div>
         <div className="contact-form">
-          <input type="email" placeholder="Votre adresse mail *" />
-          <input type="text" placeholder="Objet :" />
-          <textarea placeholder="Texte :"></textarea>
-          <button>Envoyer</button>
+          <form onSubmit={sendEmail}>
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Votre adresse mail *"
+            />
+            <input type="text" name="user_subject" placeholder="Objet :" />
+            <textarea name="message" placeholder="Texte :"></textarea>
+            <button>Envoyer</button>
+          </form>
         </div>
       </div>
     </>
